da94331e9f1e3342b907f43b500860264fd3fa80
kinanqassem87 <35800866+kinanqassem87@users.noreply.github.com>
2018-02-28 15:31:38 +0200

    Initial commit

---
+++ SendSMS/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SendSMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtxtMessage = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To : ";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(84, 64);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(212, 20);
+            this.txtTo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Message : ";
+            // 
+            // rtxtMessage
+            // 
+            this.rtxtMessage.Location = new System.Drawing.Point(84, 90);
+            this.rtxtMessage.Name = "rtxtMessage";
+            this.rtxtMessage.Size = new System.Drawing.Size(212, 96);
+            this.rtxtMessage.TabIndex = 3;
+            this.rtxtMessage.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(221, 192);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "User Name: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password : ";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(84, 38);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(212, 20);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(84, 12);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(212, 20);
+            this.txtUser.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 219);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.rtxtMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtxtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+    }
+}
+
---
+++ SendSMS/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+
+
+namespace SendSMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            SmtpClient smtp = new SmtpClient();
+            MailMessage message = new MailMessage();
+            smtp.Credentials = new NetworkCredential(txtUser.Text, txtPassword.Text);
+            smtp.Host = "ipipi.com";
+            message.From = new MailAddress(string.Format("{0}@ipipi.com", txtUser.Text));
+            message.To.Add(string.Format("{0}@sms.ipipi.com", txtTo.Text));
+            message.Subject = "Test Send Message.";
+            message.Body = rtxtMessage.Text;
+            smtp.Send(message);
+            MessageBox.Show("Sent Successfully.");
+
+            }
+            catch (Exception) { MessageBox.Show("Error"); }
+            
+            //*******************************
+
+            //for send mail 
+            //**
+            //using System.Net.Mail;
+
+
+            string fromaddr = "kinanshasho@gmail.com";
+            string toaddr = "Address";//TO ADDRESS HERE
+            string password = "Password";
+
+
+            MailMessage msg = new MailMessage();
+            msg.Subject = "Username &password";
+            msg.From = new MailAddress(fromaddr);
+            msg.Body = "Message BODY";
+            msg.To.Add(new MailAddress(toaddr));
+            SmtpClient smtp1 = new SmtpClient();
+            smtp1.Host = "smtp.gmail.com";
+            smtp1.Port = 587;
+            smtp1.UseDefaultCredentials = false;
+            smtp1.EnableSsl = true;
+            NetworkCredential nc = new NetworkCredential(fromaddr, password);
+            smtp1.Credentials = nc;
+            smtp1.Send(msg);
+        }
+    }
+}
